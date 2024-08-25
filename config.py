3bccfacc06d69b07e55217121779aa033b08f34a
Ankit <155639266+gojo22k@users.noreply.github.com>
2024-08-25 13:11:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 APP_ID = int(os.environ.get("APP_ID", "25198711"))
 API_HASH = os.environ.get("API_HASH", "2a99a1375e26295626c04b4606f72752")
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002221437806"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002151954601"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1740287480"))
@@ -25,7 +25,7 @@ IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/How_to_Download_7x/35") 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002151954601"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002221437806"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
